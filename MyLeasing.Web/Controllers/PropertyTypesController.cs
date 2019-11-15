fdf9cbc1977809d5d9f20cc0ea73b481c58bbec4
hyhsoporte <groupdevsoft@gmail.com>
2019-11-15 01:48:31 -0500

    Add Security Token

--- MyLeasing.Web/Controllers/PropertyTypesController.cs
+++ MyLeasing.Web/Controllers/PropertyTypesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,8 +11,10 @@ using MyLeasing.Web.Data.Entities;
 
 namespace MyLeasing.Web.Controllers
 {
+    [Authorize(Roles = "Manager")]
     public class PropertyTypesController : Controller
     {
+
         private readonly DataContext _context;
 
         public PropertyTypesController(DataContext context)
