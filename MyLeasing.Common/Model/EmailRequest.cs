0715ede31a65c38ae66b0f89e915bf8a5ea62123
hyhsoporte <groupdevsoft@gmail.com>
2019-11-15 00:47:11 -0500

    Owners API Controller

---
+++ MyLeasing.Common/Model/EmailRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MyLeasing.Common.Model
+{
+    public class EmailRequest
+    {
+       [Required]
+       [EmailAddress]
+       public string Email { get; set; }
+
+    }
+}
